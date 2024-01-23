9e0275edb879b626040c3cd24b325833b92b5e86
selcher_k <selcher@juno.com>
2024-01-22 19:58:39 -0500

    Display quote cards

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "./App.css";
 import { Loader } from "react-feather";
+import Quotes from "./components/quotes/Quotes";
+import "./App.css";
 
 function App() {
-  const [quotes, setQuotes] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
-    
 
-    const fetchQuotes = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(quotesUrl);
-        const results = await response.json();
-        setQuotes(results);
-      } catch (error) {
-        console.log("There was an error", error);
-      }
-      setLoading(false);
-    };
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(quotesUrl);
+      const results = await response.json();
+      setQuotes(results);
+    } catch (error) {
+      console.log("There was an error", error);
+    }
+    setLoading(false);
+  };
 
-    useEffect(() => {
-      fetchQuotes();
-    }, []);
-  
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
   return (
     <div className='App'>
       <Header />
-      <main>{loading ? < Loader/> : JSON.stringify(quotes)}</main>
+      <main>{loading ? <Loader /> : <Quotes quotes={quotes} />}</main>
       <Footer />
     </div>
   );
