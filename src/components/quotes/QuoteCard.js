81559d8939dffa5806a5c9cebd923d92303e519a
selcher_k <selcher@juno.com>
2024-01-25 18:54:50 -0500

    Filter quotes by category

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -4,6 +4,11 @@ function QuoteCard({ quote }) {
   return (
     <article className='quote-card'>
       <div>
+        <h3 className="categories">
+          {quote.categories.map((category) => (
+            <span className="category" key={category}>{category}</span>
+          ))}
+        </h3>
         <h3>{quote.text}</h3>
       </div>
       <footer>
