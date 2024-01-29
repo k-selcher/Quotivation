b8caa2cbb898648d940dc518a0a7c5ac77dac145
selcher_k <selcher@juno.com>
2024-01-29 16:32:26 -0500

    Favorite quotes part 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import "./App.css";
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [favoriteQuotes, setFavoriteQuotes] = useState([])
   const [category, setCategory] = useState("All");
+  const maxFaves = 3;
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
@@ -35,10 +37,32 @@ function App() {
     setCategory(e.target.value);
   };
 
+
+  const addToFavorites = (quoteId) => {
+    const selectedQuote = quotes.find((quote) => quote.id === quoteId);
+    
+      const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === selectedQuote.id);
+
+      if (alreadyFavorite) {
+        console.log("You already favorited this quote.")
+      } else if (favoriteQuotes.length < maxFaves) {
+      setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
+      console.log("Added to Favorites");
+    } else {
+      console.log("Max number of quotes reached. Please delete one to add a new one.");
+    }
+  };
+
   return (
     <div className='App'>
       <Header />
       <main>
+        <section className="favorite-quotes">
+          <div className="wrapper quotes">
+            <h3>Top 3 Favorite Quotes</h3>
+            {favoriteQuotes.length > 0 && JSON.stringify(favoriteQuotes)}
+          </div>
+        </section>
         {loading ? (
           <Loader />
         ) : (
@@ -47,6 +71,7 @@ function App() {
             category={category}
             categories={categories}
             handleCategoryChange={handleCategoryChange}
+            addToFavorites={addToFavorites}
           />
         )}
       </main>
